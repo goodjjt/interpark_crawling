ac6787684dd7cdda3222bf43271aec2c4c7617a3
goodjjt <goodjjt@gmail.com>
2023-05-16 17:10:17 +0900

    통합 작업

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from datetime import datetime
 import time
 from pprint import pprint
 import asyncio
+import psycopg2
+
+# https 처리 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Telegram
 async def bot_send(msg):
@@ -15,49 +19,87 @@ async def bot_send(msg):
     bot = telegram.Bot(token = telegram_token)
     await bot.sendMessage(chat_id=telegram_id, text=msg)
 
-url_array = [
-            'https://api-ticketfront.interpark.com/v1/goods/22002652/playSeq/PlaySeq/256/REMAINSEAT',
-            'https://api-ticketfront.interpark.com/v1/goods/22011899/playSeq/PlaySeq/143/REMAINSEAT',
-            'https://api-ticketfront.interpark.com/v1/goods/22005895/playSeq/PlaySeq/317/REMAINSEAT',
-             ]
 
-data_array = [
-            '노을 캠핑장 2023-05-19 https://tickets.interpark.com/goods/22002652',
-            '노을진 캠핑장 2023-05-19 https://tickets.interpark.com/goods/22011899',
-            '킨텍스 캠핑장 2023-05-19 https://tickets.interpark.com/goods/22005895'
-            ]
+# postgresSql
+conn = psycopg2.connect(database="postgres",
+                        host="goodjjt.iptime.org",
+                        user="postgres",
+                        password="wjswnsxo18!",
+                        port="25432")
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM camping_booking_info order by id")
+records = cursor.fetchall()
 
-print("[" + "interpark 예약" + "] ");
+data_array = []
+url_array = []
+site_gubun_array = []
+payload_array = []
+
+for row in records:
+    data_array.append(row[1] + " " + row[2])
+    url_array.append(row[3])
+    site_gubun_array.append(row[7])
+    payload_array.append(row[9])
+
+# url_array = [
+#             'https://api-ticketfront.interpark.com/v1/goods/22002652/playSeq/PlaySeq/256/REMAINSEAT',
+#             'https://api-ticketfront.interpark.com/v1/goods/22011899/playSeq/PlaySeq/143/REMAINSEAT',
+#             'https://api-ticketfront.interpark.com/v1/goods/22005895/playSeq/PlaySeq/317/REMAINSEAT',
+#             'https://api-ticketfront.interpark.com/v1/goods/21012652/playSeq/PlaySeq/503/REMAINSEAT'
+#              ]
+
+# data_array = [
+#             '노을 캠핑장 2023-05-19 https://tickets.interpark.com/goods/22002652',
+#             '노을진 캠핑장 2023-05-19 https://tickets.interpark.com/goods/22011899',
+#             '킨텍스 캠핑장 2023-05-19 https://tickets.interpark.com/goods/22005895',
+#             '천왕산 캠핑장 2023-05-19 https://tickets.interpark.com/goods/21012652'
+#             ]
+
+print("[" + "캠핑 예약 작업 시작" + "] ")
 for index, value in enumerate(data_array):
     print(value)
+    print(site_gubun_array[index])
+    print(payload_array[index])
 
-jsonData = None
-cnt = 0
-
-def message1():
+def crawling():
     for index, value in enumerate(url_array):
-        # BeautifulSoup
-        response = requests.get(value)
-        cnt = 0
-        message = "[" + data_array[index] + "]" + '\n'
-        if response.status_code == 200:
-            jsonData = response.json()
-            # print(jsonData)
-            for data in jsonData.get("data").get("remainSeat"):
-                message = message + data.get("seatGradeName") +  " : " + str(data.get("remainCnt")) + '\n'
-                if data.get("remainCnt") > 0:
-                    cnt += 1
-            print(time.strftime('%Y-%m-%d %H:%M:%S'), ":", cnt)
-            # print("cnt : ", message)
-            if cnt > 0:
-                asyncio.run(bot_send(message)) 
-                # bot.sendMessage(chat_id=telegram_id, text=message)
-                # bot_sst.sendMessage(chat_id=telegram_id_sst, text=message)
-        else :
-            print(response.status_code)
+        # 인터파크 티켓
+        if site_gubun_array[index] == "interpark":
+            response = requests.get(value)
+            cnt = 0
+            message = "[" + data_array[index] + "]" + '\n'
+            if response.status_code == 200:
+                jsonData = response.json()
+
+                for data in jsonData.get("data").get("remainSeat"):
+                    message = message + data.get("seatGradeName") +  " : " + str(data.get("remainCnt")) + '\n'
+                    if data.get("remainCnt") > 0:
+                        cnt += 1
+
+                print(time.strftime('%Y-%m-%d %H:%M:%S'), ":", cnt)
+                if cnt > 0:
+                    asyncio.run(bot_send(message))          
+            else :
+                print(response.status_code)
+        # 이포보 오토 캠핑장         
+        if site_gubun_array[index] == "mirihae":
+            response = requests.post(value, data=payload_array[index], verify=False)
+            cnt = 0
+            message = "[" + data_array[index] + "]" + '\n'
+            if response.status_code == 200:
+                jsonData = response.json()
+                for data in jsonData.get('pinCategoryList')[0].get('pinList'):
+                    if data.get("reserveCnt") == 0:
+                        cnt += 1
+                        message = message + data.get("itemNm") + ', '
+                print(time.strftime('%Y-%m-%d %H:%M:%S'), ":", cnt)
+                if cnt > 0:
+                    asyncio.run(bot_send(message))    
+            else :
+                print(response.status_code)        
 
 # step3.실행 주기 설정
-schedule.every(30).seconds.do(message1)
+schedule.every(10).seconds.do(crawling)
 # schedule.every(30).minutes.do(message1)
 
 while True:
